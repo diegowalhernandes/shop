0575d9dfecaf181fcaa0b27e830232d3c6452678
Diego Hernandes <diegowalhernandes@gmail.com>
2023-05-02 18:50:47 -0300

    ✨ feat:Global Styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,14 @@
+import {styled} from '../styles'
+
+const Button = styled('button', {
+    backgroundColor: '$primary'
+})
 export default function Home(){
     return(
+        <>
         <h1>Hello World</h1>
+        <Button>Enviar</Button>
+        </>
+        
     )
 }
