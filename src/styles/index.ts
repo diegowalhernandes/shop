90bc4222821bdaaf4c58f746f94a5e4640990d80
Diego Hernandes <diegowalhernandes@gmail.com>
2023-05-03 19:32:15 -0300

    ✨ feat:init slider

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,6 +1,6 @@
-import { createStitches } from "@stitches/react";
+import { createStitches } from "@stitches/react"; 
 
-export const{
+export const {
     config,
     styled,
     css,
@@ -9,22 +9,26 @@ export const{
     getCssText,
     theme,
     createTheme
+
 } = createStitches({
     theme: {
         colors: {
             primary: '#8257e6',
-
             white: '#fff',
 
             gray900: '#121214',
-            gray800: '#282024',
+            gray800: '#202024',
             gray300: '#c4c4cc',
             gray100: '#e1e1e6',
 
             green500: '#00875f',
-            green300:  '#00b37e'
-
-
+            green300: '#00b37e',
+        }, 
+        fontSizes: {
+            md: '1.125rem', 
+            lg: '1.125rem',
+            xl: '1.5rem',
+            '2xl':'2rem'
         }
     }
 })
