6a2e97c6065d041ec1378a406fd0377ab172b5b0
Diego Hernandes <diegowalhernandes@gmail.com>
2023-05-03 20:24:42 -0300

    ✨ feat:Config Setup Stripe

--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -2,8 +2,8 @@ import { styled } from "..";
 
 export const HomeContainer = styled('main', {
   display: 'flex',
-//   gap: '3rem',
-  width: '100%',
+  // gap: '3rem',
+ width: '100%',
   maxWidth: 'calc(100vw - ((100vw - 1180px) / 2))',
   marginLeft: 'auto',
   minHeight: 656
